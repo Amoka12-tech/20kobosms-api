a16d7c80ad395ff5d90ca8334d7821df9c028a7c
Abdulmutalib Amoka <amokamutalibfut@gmail.com>
2021-07-27 21:37:14 +0100

    add to comment index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import auth from './auth';
 import user from './user';
+import contact from './contact';
 
 const router = express();
 
 
     router.use('/auth', auth);
     router.use('/user', user);
+    router.use('/contact', contact);
 
 
 
